50da1eee7fc451963805d934077dd0ed865fb793
Denis <46957181+denistr16@users.noreply.github.com>
2020-04-23 01:40:05 +0300

    Backward (#1)
    * .
    
    * update grad attribute, update backward, update zero_grad

--- mini_torch/tensor.py
+++ mini_torch/tensor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 
 class Tensor:
@@ -15,13 +16,15 @@ class Tensor:
         self.prev = prev
 
         self.grad = None
-        self.back_prop_grad = {}
+        self.__default_grad_value = None
 
         if required_grad:
             if grad_value is None:
                 self.grad = {self.name: 1.}
+                self.__default_grad_value = copy.deepcopy(self.grad)
             else:
                 self.grad = grad_value
+                self.__default_grad_value = copy.deepcopy(self.grad)
 
     @classmethod
     def from_numpy(cls, name: str, data: np.array, required_grad=True) -> 'Tensor':
@@ -57,8 +60,6 @@ class Tensor:
                       required_grad=required_grad, grad_value=grad_value, prev=prev)
 
     def __sub__(self, obj):
-        # minus_one = Tensor.from_numpy(name='minus', data=-1 * np.ones(obj.shape()), required_grad=False)
-        # return Tensor.from_tensor(name='sub', tensor=self + obj * minus_one, chained=True)
         grad_value = {}
 
         if self.grad is not None:
@@ -93,13 +94,13 @@ class Tensor:
                       required_grad=required_grad, grad_value=grad_value, prev=prev)
 
     def __truediv__(self, obj):
-        return Tensor.from_tensor(name='div', tensor=self*(obj**-1), chained=True)
+        return Tensor.from_tensor(name='div', tensor=self * (obj ** -1), chained=True)
 
     def __pow__(self, value):
         grad_value = {}
 
         if self.grad is not None:
-            grad_value[self.name] = value * np.power(self.data, value-1)
+            grad_value[self.name] = value * np.power(self.data, value - 1)
 
         prev = {self.name: self}
         name = 'pow'
@@ -118,46 +119,23 @@ class Tensor:
         rep += "shape: {}\n".format(str(self.shape()))
         return rep
 
-    def backward_a(self):
-        if self.prev is None:
-            return self.grad[self.name]
-        else:
-            result = []
-            for k, v in self.prev.items():
-                if v.grad is not None:
-                    result.append(self.grad[k] * self.prev[k].backward())
-                else:
-                    pass
-            return result
-
-    def backward_b(self, d=dict()):
-        if self.prev is None:
-            d[self.name] = self.grad[self.name]
-            return d
-        else:
-
-            for name, obj in self.prev.items():
-                if obj.grad is not None:
-                    d = self.prev[name].backward_b(d)
-                    if name in d.keys():
-                        d[name] = d[name] * self.grad[name]
-
-                else:
-                    pass
-            return d
-
-    # TODO write zero_grad()
     def backward(self):
         if self.prev is None:
-            self.back_prop_grad[self.name] = self.grad[self.name]
-            return self.back_prop_grad
-        else:
+            return
+
+        for prev_name, prev_obj in self.prev.items():
+            if prev_obj.grad is not None:
+                for k, v in prev_obj.grad.items():
+                    prev_obj.grad[k] = v * self.grad[prev_name]
+                prev_obj.backward()
+            else:
+                pass
+        return
+
+    def zero_grad(self):
+        self.grad = self.__default_grad_value
+
+        if self.prev is not None:
             for prev_name, prev_obj in self.prev.items():
-                if prev_obj.grad is not None:
-                    back_prop_grad = prev_obj.backward()
-                    for k, v in back_prop_grad.items():
-                        back_prop_grad[k] = v * self.grad[prev_name]
-                    self.back_prop_grad.update(back_prop_grad)
-                else:
-                    pass
-            return self.back_prop_grad
+                prev_obj.zero_grad()
+        return
