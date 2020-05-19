1507efdf1b344b2c38c6662205541c41900765c7
denistr16 <troyanov.denis@gmail.com>
2020-05-19 23:14:11 +0300

    add grad test

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,4 +1,5 @@
 import unittest
+import torch
 
 from mini_torch.tensor import Tensor as T
 from tests.unit_test_data import test_data
@@ -38,11 +39,30 @@ class MathTest(unittest.TestCase):
         self.assertEqual(f.data, f_t)
 
     def test_poly(self):
-        f = w0 + w1 ** 2 * x1 - w2**2 * x2
-        f_t = test_data['w0'] + test_data['w1']**2 * test_data['x1'] - \
-              test_data['w2']**2 * test_data['x2']
+        f = w0 + w1 ** 2 * x1 - w2 ** 2 * x2
+        f_t = test_data['w0'] + test_data['w1'] ** 2 * test_data['x1'] - test_data['w2'] ** 2 * test_data['x2']
 
         self.assertEqual(f.data, f_t)
+
+
+class GradTest(unittest.TestCase):
+    def test_grad(self):
+        f = w0 + w1 ** 2 * x1 - w2 ** 2 * x2
+        f.zero_grad()
+        f.backward()
+
+        t_w0 = torch.tensor(test_data['w0'], requires_grad=True)
+        t_w1 = torch.tensor(test_data['w1'], requires_grad=True)
+        t_w2 = torch.tensor(test_data['w2'], requires_grad=True)
+        t_x1 = torch.tensor(test_data['x1'])
+        t_x2 = torch.tensor(test_data['x2'])
+
+        t_y = t_w0 + t_w1 ** 2 * t_x1 - t_w2 ** 2 * t_x2
+        t_y.backward()
+
+        self.assertEqual(w0.grad['w0'], t_w0.grad.data.numpy())
+        self.assertEqual(w1.grad['w1'], t_w1.grad.data.numpy())
+        self.assertEqual(w2.grad['w2'], t_w2.grad.data.numpy())
 
 
 if __name__ == '__main__':
